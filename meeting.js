b59ad5c4fa4a80a1f76ac800fd737d80d89488db
Henric Englund <hnrcnglnd@gmail.com>
2015-04-22 19:41:17 +0200

    Salary -> Cost. Attendants -> Attendees

--- meeting.js
+++ meeting.js
@@ -1,10 +1,10 @@
-var MAX_SALARY = 2000;
-var MAX_ATTENDANTS = 100;
+var MAX_COST = 2000;
+var MAX_ATTENDEES = 100;
 
 var Cost = new (function() {
   var $current,
-  salary = 0,
-  attendants = 0,
+  cost = 0,
+  attendees = 0,
   incrementTime = 70,
   currentTime = 0,
   init = function() {
@@ -16,32 +16,32 @@ var Cost = new (function() {
     currentTime += incrementTime / 10;
   },
   formatCost = function(time) {
-    return attendants * salary / 60 / 60 * time / 100;
+    return attendees * cost / 60 / 60 * time / 100;
   };
-  this.setSalary = function(s) {
-    salary = s;
+  this.setCost = function(c) {
+    cost = c;
   };
-  this.setAttendants = function(a) {
-    attendants = a;
+  this.setAttendees = function(a) {
+    attendees = a;
   };
   $(init);
 });
 
 $(document).ready(function() {
-  new Dragdealer("salary-slider", {
-    x: queryValue("salary", MAX_SALARY, 0.1),
+  new Dragdealer("cost-slider", {
+    x: queryValue("cost", MAX_COST, 0.1),
     animationCallback: function(x, y) {
-      var val = Math.round(x * MAX_SALARY);
-      $("#salary-slider .value").text(val);
-      Cost.setSalary(val);
+      var val = Math.round(x * MAX_COST);
+      $("#cost-slider .value").text(val);
+      Cost.setCost(val);
     }
   });
-  new Dragdealer("attendants-slider", {
-    x: queryValue("attendants", MAX_ATTENDANTS, 0.1),
+  new Dragdealer("attendees-slider", {
+    x: queryValue("attendees", MAX_ATTENDEES, 0.1),
     animationCallback: function(x, y) {
-      var val = Math.round(x * MAX_ATTENDANTS);
-      $("#attendants-slider .value").text(val);
-      Cost.setAttendants(val);
+      var val = Math.round(x * MAX_ATTENDEES);
+      $("#attendees-slider .value").text(val);
+      Cost.setAttendees(val);
     }
   });
   $("#current").fitText(0.5);
